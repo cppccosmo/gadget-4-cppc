7c2dde592c92afa74d7ffc5415f4e6a38594be88
Giovanni Pierobon <g.pierobon@unsw.edu.au>
2023-02-17 15:09:25 +1100

    Added velocity spectrum scripts, to inegrate with htools

--- htools/scripts/spectrum.py
+++ htools/scripts/spectrum.py
@@ -1,6 +1,69 @@
 import numpy as np
 
+
+'''
+TO ORGANIZE
+'''
+
+def PS_theta(fname,ptype):
+    '''
+    Computes the power spectrum of the divergence of a 3D velocity field
+    As input pass the snapshot name, without the .hdf5 extension, and the particle type
+    Returns k,Pk,Nmodes
+    '''
+    f       = h5.File(fname+'.hdf5','r')
+    N       = f['Header'].attrs['NumPart_Total'][ptype]
+    n       = int(np.ceil(N**(1/3)))
+    BoxSize = f['Header'].attrs['BoxSize']
+    grid    = f['Parameters'].attrs['GridSize']
+    MAS     = 'CIC'  
+    axis    = 0
+    threads = 16
+
+    Vx = np.array(f['PartType%d/Velocities'%ptype][:,0]).reshape((n,n,n)).astype('float32')
+    Vy = np.array(f['PartType%d/Velocities'%ptype][:,1]).reshape((n,n,n)).astype('float32')
+    Vz = np.array(f['PartType%d/Velocities'%ptype][:,2]).reshape((n,n,n)).astype('float32')
+
+    k, Pk, Nmodes = PKL.Pk_theta(Vx,Vy,Vz,BoxSize,axis,MAS,threads)
+    return k,Pk,Nmodes
+
+
+
+def PS_momentum(fname,ptype):
+    '''
+    Computes the density constrast and momentum auto- and cross-power spectra
+    As input pass the snapshot name, without the .hdf5 extension, and the particle type
+    Returns k,Pk_dd,Pk_tt,Pk_dt,Nmodes, where:
+            Pk_dd is the standard power spectrum in (Mpc/h)^3
+            Pk_tt is the momentum auto-power spectrum in (km/s)^2*(Mpc/h)^3
+            Pk_dt is the density-momentum cross-power spectrum in (km/s)*(Mpc/h)^3
+    '''
+    f       = h5.File(fname+'.hdf5','r')
+    N       = f['Header'].attrs['NumPart_Total'][ptype]
+    n       = int(np.ceil(N**(1/3)))
+    BoxSize = f['Header'].attrs['BoxSize']
+    grid    = f['Parameters'].attrs['GridSize']
+    MAS     = 'CIC'  
+    axis    = 0
+    threads = 16
+
+    delta = MASL.density_field_gadget(fname, [ptype], grid, MAS, do_RSD=False, axis=0, verbose=True)
+    delta /= np.mean(delta, dtype=np.float32);  delta -= 1.0
+
+    Vx = np.array(f['PartType%d/Velocities'%ptype][:,0]).reshape((n,n,n)).astype('float32')
+    Vy = np.array(f['PartType%d/Velocities'%ptype][:,1]).reshape((n,n,n)).astype('float32')
+    Vz = np.array(f['PartType%d/Velocities'%ptype][:,2]).reshape((n,n,n)).astype('float32')
+
+    k, Pk_dd, Pk_tt, Pk_dt, Nmodes = PKL.XPk_dv(delta, Vx, Vy, Vz, BoxSize, axis, MAS, threads)
+    return k,Pk_dd,Pk_tt,Pk_dt,Nmodes
+
+    
+
 def total_Pnu(streams,converted_list,converted_pk_list, Nstreams):
+    '''
+    Computes the neutrino delta power spectrum ...
+    TO FINISH
+    '''
     assert len(converted_list) == len(converted_pk_list)
     num_particle_conv = len(converted_list)
     num_streams_in_particle = np.ones(num_particle_conv)
@@ -37,8 +100,4 @@ def total_Pnu(streams,converted_list,converted_pk_list, Nstreams):
     returnarray[:,1] = total_delta**2
     
     return returnarray
-
-
-
-def velocity_PS():
 
