a11fd97b1b85bf77eaccdd03f27a3ce09d0dee34
Giovanni Pierobon <g.pierobon@unsw.edu.au>
2023-04-13 16:18:18 +1000

    added nu05 in cosmologies script

--- htools/scripts/cosmologies.sh
+++ htools/scripts/cosmologies.sh
@@ -16,23 +16,26 @@ case $1 in
         COSMO_TCMBK=2.7255               # CMB temperature today in K
         COSMO_W0=-1.0                    # Dark energy equation of state 
         COSMO_WA=0.0                     
-    ;;
-
-    nu02)
-        
-    ;;
     
-    nu03)
-        
-    ;;
-    
-    nu04)
-        
     ;;
     
     nu05)
-        
+
+        COSMO_MHDM=0.052135              # HDM mass
+        COSMO_THDMK=1.95280830923360     # HDM component temperature today in K
+        COSMO_FHDM=psd/f_fd0.dat         # HDM distribution function to pass for GLR
+        COSMO_NS=0.96                    # Primordial spectral index n_s
+        COSMO_SIG8=0.79                  # Sigma8
+        COSMO_H0=0.67                    # Hubble paramter today in units of 100 km/s/Mpc
+        COSMO_OM=0.3190020049            # Omega_m for all matter
+        COSMO_OB=0.04900868              # Omega_b for baryons
+        COSMO_OH=0.0038093               # Omega_h for the HDM component
+        COSMO_TCMBK=2.7255               # CMB temperature today in K
+        COSMO_W0=-1.0                    # Dark energy equation of state
+        COSMO_WA=0.0                            
+    
     ;;
+
 esac
 
 
