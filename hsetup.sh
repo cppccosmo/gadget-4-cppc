84c84f42c663b330c14c4ff29210e4117bf6c9b2
Giovanni Pierobon <g.pierobon@unsw.edu.au>
2023-04-19 16:48:25 +1000

    hsetup minor restructure

--- hsetup.sh
+++ hsetup.sh
@@ -10,7 +10,7 @@ export MuFLRPath=$cdir/MF
 export ClassPath=/home/z5278074/class_public
 
 # Load modules
-source load_modules.sh 
+source load_modules.sh $2
 
 ########################################################
 
@@ -19,25 +19,56 @@ if [[ ! -d "$ClassPath" ]]; then
     exit 1
 fi
 
-if [[ -d $2 ]];
-    then echo 'Directory' $2 'already exists!'
+if [[ -d $1 ]];
+    then echo 'Directory' $1 'already exists!'
     exit 1
 else
-    mkdir $2
+    mkdir $1
 fi
 
-case $1 in 
-    LR)
-        source htools/scripts/build_LR.sh $2
+while true; do
+  read -p 'Do you want to restart a MultiFluid simulation? [y/n] ' RES_MF
+  case $RES_MF in
+    [yY]) break;;
+    [nN]) break;;
+    *) echo "Please enter only 'y' or 'n'.";;
+  esac
+done
+
+while true; do
+  read -p 'Do you want to restart a Hybrid simulation? [y/n] ' RES_HYB
+  case $RES_HYB in
+    [yY]) break;;
+    [nN]) break;;
+    *) echo "Please enter only 'y' or 'n'.";;
+  esac
+done
+
+case $RES_HYB in
+    y)
+        case $RES_MF in 
+            y)
+                echo 'Preparing LR, MF and Hybrid'
+                source htools/scripts/build_full.sh # Hybrid restart and MF restart
+            ;;
+            n) 
+                echo 'Preparing LR and Hybrid'
+                source htools/scripts/build_HYB.sh # Hybrid restart only
+            ;;
+        esac
     ;;
-    MF)
-        source htools/scripts/build_MF.sh $2
-    ;;
-    GLR)
-        source htools/scripts/build_GLR.sh $2
-    ;;
-    HYB)
-        source htools/scripts/build_HYB.sh $2
+    n)
+        case $RES_MF in 
+            y)
+                echo 'Preparing LR and MF'
+                source htools/scripts/build_MF.sh # MF restart only
+            ;;
+            n) 
+                echo 'Preparing LR'
+                source htools/scripts/build_LR.sh # LR only
+            ;;
+        esac
     ;;
 esac
+
 
